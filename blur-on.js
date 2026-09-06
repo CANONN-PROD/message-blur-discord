13ceb507ad9cb5b733a5b0e2766dca88850b0dbd
CANONN-PROD <canonn.prod@gmail.com>
2026-09-05 22:18:35 -0400

    Update blur-on.js

--- blur-on.js
+++ blur-on.js
@@ -1,1 +1,1 @@
-javascript:(()=>{const s=document.createElement('style');s.id='discord-blur-style';s.textContent='main{filter:blur(5px)!important}';document.head.appendChild(s)})()
+const s=document.createElement('style');s.id='discord-blur-style';s.textContent='main{filter:blur(5px)!important}';document.head.appendChild(s)
